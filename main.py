70fedc7765f220f7e795573b18b76d5f4822d017
Carl Nordin <cano2201@student.miun.se>
2022-09-01 13:10:50 +0200

    Added if, else with input

--- main.py
+++ main.py
@@ -1,1 +1,17 @@
-nr_apples = input("How many apples have you eaten today?")
+name = input("What's your name?: ")
+print("Hello", name, ":D")
+
+nr_apples = input("How many apples have you eaten today?: ")
+if int(nr_apples) == int(0):
+    print("Really? I think you should really eat one then :)")
+
+if int(nr_apples) == int(1):
+    print("While that is good, you really should eat one more.")
+
+else:
+    print("Good!" "Eating two apples a day helps to reduce cholesterol "
+          "and fight heart disease, according to research.")
+
+print("Eating two apples a day helps to reduce cholesterol "
+      "and fight heart disease, "
+      "according to research.")
